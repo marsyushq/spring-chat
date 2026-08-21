1688f649880b228308f2ade7b1e90344c5b83a1d
sk_yuliia <skrypnichenkoju@gmail.com>
2026-08-21 16:45:55 -0400

    feat: add user creation endpoint

---
+++ springchat/src/main/java/com/marsyushq/springchat/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.marsyushq.springchat.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marsyushq.springchat.dto.CreateUserRequest;
+import com.marsyushq.springchat.dto.UserResponse;
+import com.marsyushq.springchat.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public UserResponse createUser(@RequestBody CreateUserRequest req){
+        return userService.createUser(req); 
+    }
+}
---
+++ springchat/src/main/java/com/marsyushq/springchat/service/UserService.java
@@ -0,0 +1,29 @@
+package com.marsyushq.springchat.service;
+
+import org.springframework.stereotype.Service;
+
+import com.marsyushq.springchat.repository.UserRepository;
+import com.marsyushq.springchat.dto.CreateUserRequest;
+import com.marsyushq.springchat.dto.UserResponse;
+import com.marsyushq.springchat.model.User;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository; 
+    }
+
+    public UserResponse createUser(CreateUserRequest req){
+        User user = new User();
+
+        user.setUsername(req.getUsername());
+        user.setEmail(req.getEmail());
+        user.setPassword(req.getPassword());
+
+        User savedUser = userRepository.save(user);
+
+        return new UserResponse(savedUser.getId(),savedUser.getUsername(),savedUser.getEmail());
+    }
+}
