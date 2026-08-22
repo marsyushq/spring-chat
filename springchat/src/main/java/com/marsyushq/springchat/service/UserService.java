1bfa16d7126c3dc77dab4bc1213070f08e0cb2f0
sk_yuliia <skrypnichenkoju@gmail.com>
2026-08-22 05:27:00 -0400

    feat: hash user passwords with BCrypt

--- springchat/src/main/java/com/marsyushq/springchat/service/UserService.java
+++ springchat/src/main/java/com/marsyushq/springchat/service/UserService.java
@@ -1,5 +1,6 @@
 package com.marsyushq.springchat.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.marsyushq.springchat.repository.UserRepository;
@@ -10,9 +11,11 @@ import com.marsyushq.springchat.model.User;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository; 
+        this.passwordEncoder = passwordEncoder; 
     }
 
     public UserResponse createUser(CreateUserRequest req){
@@ -20,7 +23,7 @@ public class UserService {
 
         user.setUsername(req.getUsername());
         user.setEmail(req.getEmail());
-        user.setPassword(req.getPassword());
+        user.setPassword(passwordEncoder.encode(req.getPassword()));
 
         User savedUser = userRepository.save(user);
 
