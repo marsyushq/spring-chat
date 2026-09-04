f9ab826fd3d186d653cfa9a1afeabb59b42ae5c0
sk_yuliia <skrypnichenkoju@gmail.com>
2026-09-03 23:29:18 -0400

    feat: implement message API

---
+++ springchat/src/main/java/com/marsyushq/springchat/dto/CreateMessageRequest.java
@@ -0,0 +1,13 @@
+package com.marsyushq.springchat.dto;
+
+public class CreateMessageRequest {
+    private String content;
+    
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+}
